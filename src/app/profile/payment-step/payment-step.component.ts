ed9769c93ae6d6fa5fc3f85a3e114c1e1a7e0495
ashwanizsktm <ashwanizskt@gmail.com>
2021-12-20 01:43:43 +0530

    files added

--- src/app/profile/payment-step/payment-step.component.ts
+++ src/app/profile/payment-step/payment-step.component.ts
@@ -1,131 +1,145 @@
 import {
-  AfterViewChecked,
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  Input,
-  OnChanges,
-  OnInit,
-  Output,
-  QueryList,
-  TemplateRef,
-  ViewChild,
-  ViewChildren,
+    AfterViewChecked,
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    Component,
+    ElementRef,
+    Input,
+    OnChanges,
+    OnInit,
+    Output,
+    QueryList,
+    TemplateRef,
+    ViewChild,
+    ViewChildren,
 } from '@angular/core';
 
 @Component({
-  selector: 'app-payment-step',
-  templateUrl: './payment-step.component.html',
-  styleUrls: ['./payment-step.component.scss'],
-  // changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-payment-step',
+    templateUrl: './payment-step.component.html',
+    styleUrls: ['./payment-step.component.scss'],
+    // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PaymentStepComponent implements OnInit, AfterViewInit, OnChanges {
-  successIconSrc =
-    'https://cdn3.iconfinder.com/data/icons/flat-actions-icons-9/792/Tick_Mark_Dark-512.png';
+export class PaymentStepComponent implements OnInit,  OnChanges {
+    successIconSrc ='https://cdn3.iconfinder.com/data/icons/flat-actions-icons-9/792/Tick_Mark_Dark-512.png';
 
-  prevBtn: boolean = true;
-  nextBtn = true;
-  prevInitialIdx!: number;
-  prevlastIdx!: number;
-  nextInitialIdx!: number;
-  nextLastIdx!: number;
+    prevBtnIndex!: number;
+    nextBtnIndex!: number;
+    hidePrevBtn: boolean = false;
+    hideNextBtn: boolean = false;
 
-  @Input() PaymentData!: any[];
-  @Input() closedStatus!: boolean;
+    @Input() paymentData!: any[];
+    initialCashflow: any[] = [];
 
-  tempArr:any[] = [];
-  datesEle!: Element[];
+    @Input() closedStatus!: boolean;
+    itemCountAfterNextPay = 0;
 
-  constructor() {}
+    constructor() { }
 
-  ngOnInit() {
-   this.setinititalCashflow();
-   this.cashflowStatus();
+
+    ngOnInit() {
+      this.setinititalCashflow();
+      this.setCashflowStatus();
   }
-
-  prevData:any[] = [];
-  nextData:any[] = [];
-  nextDataLenght!: any;
-  predDataLenght!:number;
 
   setinititalCashflow() {
-    const now: Date = new Date();
-    let closest: any = Infinity;
-    this.PaymentData.map((item, idx )=> {
-      const date: Date = new Date(item.date);
-      if (date >= now && (date < new Date(closest) || date < closest)) {
-        closest = item.date;
-        let n = idx;
-        this.prevInitialIdx = n - 2;
-        this.prevlastIdx = n;
-        this.nextInitialIdx = n - 2;
-        this.nextLastIdx = n + 2;
-        this.tempArr = this.PaymentData.slice(n-2, n +3);
+      if (this.paymentData.length <= 5) {
+          this.hidePrevBtn = true;
+          this.hideNextBtn = true;
+          this.initialCashflow = this.paymentData.slice(0, this.paymentData.length);
+      } else {
+          const now: Date = new Date();
+          let closest: any = Infinity;
+
+          this.paymentData.forEach((item, index) => {
+              const date: Date = new Date(item.date);
+              if (date >= now && (date < new Date(closest) || date < closest)) {
+                  closest = item.date;
+                  let nextPayDateId = index;
+                  this.prevBtnIndex = nextPayDateId;
+                  this.nextBtnIndex = nextPayDateId;
+                  this.initialCashflow = this.paymentData.slice(nextPayDateId - 2, nextPayDateId + 3);
+                  switch (nextPayDateId) {
+                      case 0:
+                          this.initialCashflow = this.paymentData.slice(0, nextPayDateId + 5);
+                          console.log("next pay id is 0", nextPayDateId, this.initialCashflow);
+                          break;
+                      case 1:
+                          this.initialCashflow = this.paymentData.slice(0, nextPayDateId + 4);
+                          console.log("next pay id is 1", nextPayDateId, this.initialCashflow);
+                          break;
+                        case (nextPayDateId):
+                          if (nextPayDateId >= 6) {
+                              let secondLastIndex = this.paymentData.length - 2;
+                              if (nextPayDateId >= secondLastIndex) {
+                                  this.initialCashflow = this.paymentData.slice(secondLastIndex - 3, secondLastIndex + 3);
+                              }
+                          }
+                        break;
+                        default:
+                  }
+                  if (this.prevBtnIndex <= 2) this.hidePrevBtn = true;
+                  this.itemCountAfterNextPay = (this.paymentData.length - (index + 1));
+                  if(this.itemCountAfterNextPay <=2) this.hideNextBtn = true;
+              }
+
+          })
       }
-    })
   }
 
-  cashflowStatus(){
-    const now:Date = new Date()
-    this.PaymentData.map((list) => {
-      if (new Date(list.date) < now) list['isPaymentDone'] = true;
-       else list['isPaymentDone'] = false;
-      });
-  }
+
 
   showPrev() {
-    this.nextBtn = true;
-    let first = this.prevInitialIdx - 2;
-    let last = this.prevlastIdx + 3;
-    if (first <= 0) {
-      first = 0;
-      this.prevBtn = false;
+    if(this.itemCountAfterNextPay <=2){
+      this.hideNextBtn = true;
+      console.log(this.itemCountAfterNextPay);
+    } else{
+      this.hideNextBtn = false;
     }
-    this.tempArr = this.PaymentData.slice(first, last)
-    this.prevInitialIdx = first;
+      let first = 0;
+      let last = this.prevBtnIndex + 3;
+      this.initialCashflow = this.paymentData.slice(first, last);
+      if (first <= 0) this.hidePrevBtn = true;
   }
 
   showNext() {
-    this.nextDataLenght = this.nextDataLenght - 3;
-    console.log("on showmore,", this.nextDataLenght);
-    this.prevBtn = true;
-    let first = this.nextInitialIdx;
-    let last = this.nextLastIdx + 3;
-    if (last > this.PaymentData.length) {
-      this.nextBtn = false;
-    }
-    if(this.nextDataLenght <= 0) this.nextDataLenght = '';
-    this.tempArr = this.PaymentData.slice(first, last);
-    this.nextLastIdx = last;
+    let first;
+      if(this.nextBtnIndex <= 2)first  = 0;
+        else first = this.nextBtnIndex - 2;
+      let last = this.paymentData.length;
+      this.initialCashflow = this.paymentData.slice(first, last);
+      if (last >= this.paymentData.length) this.hideNextBtn = true;
+      if (this.prevBtnIndex <= 2) this.hidePrevBtn = true;
+      else this.hidePrevBtn = false;
   }
 
   setCashflowStatus() {
     const now: Date = new Date();
     let closest: any = Infinity;
-    let datesEle = Array.from(
-      document.querySelectorAll('.payment-order__list--item-date')
-    );
-    datesEle.forEach((el: any) => {
-      const date: Date = new Date(el.textContent);
-      if (date >= now && (date < new Date(closest) || date < closest)) {
-        closest = el.textContent;
-        el.parentElement.classList.add('active-item');
-      }
-    });
-  }
+    if (this.paymentData.length > 0) {
+        this.paymentData.forEach((item, index) => {
+            item['serialNo'] = index + 1;
+            const date: Date = new Date(item.date);
+            if (date >= now && (date < new Date(closest) || date < closest)) {
+                closest = item.date;
+                item['active'] = true;
+            }
 
-  ngAfterViewInit() {
-    this.setCashflowStatus();
-  }
-  ngOnChanges() {
-    if(this.closedStatus == true) {
-      this.nextBtn = true;
-      this.prevBtn = true;
-    } else if(this.closedStatus == false){
-      this.setinititalCashflow();
-      this.nextBtn = true;
-      this.prevBtn = true;
+            if (new Date(item.date) < now) item['isPaymentDone'] = true;
+            else item['isPaymentDone'] = false;
+        });
     }
+}
+
+  ngOnChanges() {
+      if (this.closedStatus == true) {
+          this.hideNextBtn = false;
+          this.hidePrevBtn = false;
+      } else if (this.closedStatus == false) {
+          this.hideNextBtn = false;
+          this.hidePrevBtn = false;
+      }
+      this.setinititalCashflow();
+      this.setCashflowStatus();
   }
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -10,41 +10,57 @@ export class ProfileComponent {
 
   constructor() {}
   // start of dummy data
-  theData = [
-    { id: 1, date: '21 Jan 2021', amount: '27056.00' },
-    { id: 2, date: '21 Feb 2021', amount: '27056.00' },
-    { id: 3, date: '21 Mar 2021', amount: '27056.00' },
-    { id: 4, date: '21 Apr 2021', amount: '27056.00' },
-    { id: 5, date: '21 May 2021', amount: '27056.00' },
-    { id: 6, date: '21 Jun 2021', amount: '27056.00' },
-    { id: 7, date: '21 Jul 2021', amount: '27056.00' },
-    { id: 8, date: '21 Aug 2021', amount: '27056.00' },
-    { id: 9, date: '21 sep 2021', amount: '27056.00' },
-    { id: 10, date: '21 Oct 2021', amount: '27056.00' },
-    { id: 11, date: '21 Nov 2021', amount: '27056.00' },
-    { id: 12, date: '21 Dec 2021', amount: '27056.00' },
-    { id: 13, date: '21 Jan 2022', amount: '27056.00' },
-    { id: 14, date: '21 Feb 2022', amount: '27056.00' },
-    { id: 15, date: '21 Mar 2022', amount: '27056.00' },
-    { id: 16, date: '21 Apr 2022', amount: '27056.00' },
-    { id: 17, date: '21 May 2022', amount: '27056.00' },
-    { id: 18, date: '21 Jun 2022', amount: '27056.00' },
-    { id: 19, date: '21 Jul 2022', amount: '27056.00' },
-    { id: 20, date: '21 Aug 2022', amount: '27056.00' },
-    { id: 21, date: '21 sep 2022', amount: '27056.00' },
-    { id: 22, date: '21 Oct 2022', amount: '27056.00' },
-    { id: 23, date: '21 Nov 2022', amount: '27056.00' },
-    { id: 24, date: '21 Dec 2022', amount: '27056.00' },
-  ];
+
+    ipSchedule = [
+      // {date: '21-Jan-2020', amount: '27056.00' },
+      // {date: '21-Feb-2020', amount: '27056.00' },
+      // {date: '21-Mar-2020', amount: '27056.00' },
+      // {date: '21-Apr-2020', amount: '27056.00' },
+      // {date: '21-May-2020', amount: '27056.00' },
+      // {date: '21-Jun-2020', amount: '27056.00' },
+      // {date: '21-Jul-2020', amount: '27056.00' },
+      // {date: '21-Aug-2020', amount: '27056.00' },
+      // {date: '21-sep-2020', amount: '27056.00' },
+      // {date: '21-Oct-2020', amount: '27056.00' },
+      // {date: '21-Nov-2020', amount: '27056.00' },
+      // {date: '21-Dec-2020', amount: '27056.00' },
+
+      // {date: '21-Jan-2021', amount: '27056.00' },
+      // {date: '21-Feb-2021', amount: '27056.00' },
+      {date: '21-Mar-2021', amount: '27056.00' },
+      {date: '21-Apr-2021', amount: '27056.00' },
+      {date: '21-May-2021', amount: '27056.00' },
+      {date: '21-Jun-2021', amount: '27056.00' },
+      {date: '21-Jul-2021', amount: '27056.00' },
+      {date: '21-Aug-2021', amount: '27056.00' },
+      {date: '21-sep-2021', amount: '27056.00' },
+      {date: '21-Oct-2021', amount: '27056.00' },
+      {date: '21-Nov-2021', amount: '27056.00' },
+      {date: '21-Dec-2021', amount: '27056.00' },
+      {date: '21-Jan-2022', amount: '27056.00' },
+      {date: '21-Feb-2022', amount: '27056.00' },
+      {date: '21-Mar-2022', amount: '27056.00' },
+      {date: '21-Apr-2022', amount: '27056.00' },
+      {date: '21-May-2022', amount: '27056.00' },
+      {date: '21-Jun-2022', amount: '27056.00' },
+      {date: '21-Jul-2022', amount: '27056.00' },
+      {date: '21-Aug-2022', amount: '27056.00' },
+      {date: '21-sep-2022', amount: '27056.00' },
+      {date: '21-Oct-2022', amount: '27056.00' },
+      {date: '21-Nov-2022', amount: '27056.00' },
+      // {date: '21-Dec-2022', amount: '27056.00' }
+    ]
+
+
   // End of dummy data
-
-
-
-  SliderStatusInfo!:{isShow:string, isSlideOut: boolean }
+   theFlag = false;
+  SliderStatusInfo!:any;
   openCashFlow() {
       this.SliderStatusInfo = {
         isShow: 'flex',
         isSlideOut: false
       }
+      this.theFlag = true;
     }
+
   }
